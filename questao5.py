bcddee87d971cb986ee5b19e3d23f73f58c115e8
myllasousa <143356315+myllasousa@users.noreply.github.com>
2023-09-17 14:02:51 -0300

    Delete questao5.py

--- questao5.py
+++
@@ -1,8 +0,0 @@
-preco = float(input(""))
-preco_avista = preco - (preco * 9 / 100)
-preco_5v = preco / 5
-preco_10v = preco / 10
-preco_10v2 = preco_10v + preco_10v * 17 / 100
-print(f'{preco_avista:.2f}')
-print(f'{preco_5v:.2f}')
-print(f'{preco_10v2:.2f}')
